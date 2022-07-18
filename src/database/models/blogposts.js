72ba82952a73b658dc67800c8273468bfa5df852
Lucas <bithome1@outlook.com>
2022-07-18 18:31:45 -0300

    adicionaod fontes de pesquisa

--- src/database/models/blogposts.js
+++ src/database/models/blogposts.js
@@ -1,4 +1,10 @@
-'use strict';
+/** source: https://app.betrybe.com/course/back-end/nodejs-orm-autenticacao/orm-interface-da-aplicacao-com-o-banco-de-dados/d0fc385e-b0ce-4b3d-8246-779d5dc13682/conteudos/087fdb7c-330e-4299-97e1-efe1692ab8dc/model/8e9750a0-360a-40a2-96e1-12f759c8d808?use_case=side_bar 
+ * 
+ * source: https://app.betrybe.com/course/live-lectures/sd-cohort-19-b#aula-241-orm-interface-da-aplicacao-com-o-banco-de-dados
+*/
+
+
+//'use strict';
 // const {
 //   Model
 // } = require('sequelize');
