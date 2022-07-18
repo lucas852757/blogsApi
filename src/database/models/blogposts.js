2d781c4f27eac739bed120746aa57d5d11b1c685
Lucas <bithome1@outlook.com>
2022-07-18 18:18:04 -0300

    classe -> função

--- src/database/models/blogposts.js
+++ src/database/models/blogposts.js
@@ -1,24 +1,42 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class BlogPosts extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   BlogPosts.init({
+//     title: DataTypes.STRING,
+//     content: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'BlogPosts',
+//   });
+//   return BlogPosts;
+// };
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: {
+      type: DataTypes.STRING
+    },
+    content: {
+      type: DataTypes.STRING
     }
-  };
-  BlogPosts.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'BlogPosts',
-  });
-  return BlogPosts;
+  })
 };
+
+module.exports = BlogPost;
