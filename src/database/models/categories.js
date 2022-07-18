60fdeb64f357d1eab92802202154dbffe2374b98
Lucas <bithome1@outlook.com>
2022-07-18 18:09:20 -0300

    adicionado return e module.exports

--- src/database/models/categories.js
+++ src/database/models/categories.js
@@ -31,4 +31,7 @@ const Category = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     name: DataTypes.STRING
   })
-}
+  return Category;
+};
+
+module.exports = Category;
