b277ab663883f8934a973bc52886efe8982edbd2
Lucas <bithome1@outlook.com>
2022-07-18 14:45:12 -0300

    adicionado campos

--- src/database/migrations/20220718164922-Users.js
+++ src/database/migrations/20220718164922-Users.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   up: async (queryInterface, Sequelize) => {
     /**
      * Add altering commands here.
@@ -8,8 +12,40 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTERGER
+      },
+      displayName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.databaseVersion
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATA
+      }
+    })
   },
 
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   down: async (queryInterface, Sequelize) => {
     /**
      * Add reverting commands here.
@@ -17,5 +53,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable('Users')
   }
 };
