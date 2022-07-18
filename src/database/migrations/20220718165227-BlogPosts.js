38f95892cd2d56c6aee5c776cec6c876f443cb21
Lucas <bithome1@outlook.com>
2022-07-18 15:50:09 -0300

    adicionado campos

--- src/database/migrations/20220718165227-BlogPosts.js
+++ src/database/migrations/20220718165227-BlogPosts.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   up: async (queryInterface, Sequelize) => {
     /**
      * Add altering commands here.
@@ -8,8 +12,31 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        type: Sequelize.DATA,
+      },
+      updatedAt: {
+        type: Sequelize.DATA
+      }
+    });
   },
 
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   down: async (queryInterface, Sequelize) => {
     /**
      * Add reverting commands here.
