d0950a8a097eb999e2c5386528b85555a806aa88
Lucas <bithome1@outlook.com>
2022-07-18 21:38:10 -0300

    classe -> função

--- src/database/models/postcategories.js
+++ src/database/models/postcategories.js
@@ -1,24 +1,49 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class PostCategories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class PostCategories extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   PostCategories.init({
+//     postId: DataTypes.INTEGER,
+//     categoryId: DataTypes.INTEGER
+//   }, {
+//     sequelize,
+//     modelName: 'PostCategories',
+//   });
+//   return PostCategories;
+// };
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',{
+   
+    postId: {
+      allowNull: false,
+      primaryKey: false,
+      type: DataTypes.INTEGER
+    },
+    categoryId: {
+      allowNull: false,
+      primaryKey: false,
+      type: DataTypes.INTEGER
     }
-  };
-  PostCategories.init({
-    postId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'PostCategories',
   });
-  return PostCategories;
-};
+
+  return PostCategory;
+}
+
+module.exports = PostCategory;
