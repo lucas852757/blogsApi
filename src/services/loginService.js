bff959582af6dde5938a368c2a618cf4d0c23c37
Lucas <bithome1@outlook.com>
2022-07-23 14:25:28 -0300

    adicionado Joi

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,19 @@
-const models = require('../database/models');
+/* source: https://stackoverflow.com/questions/48720942/node-js-joi-how-to-display-a-custom-error-messages */
+const Joi = require('joi');
+// const models = require('../database/models');
 
 const login = async ({ email, password }) => {
-  console.log(email);
- const resonse = await models.User.create({ email, password });
- return resonse;
+  const schema = Joi.object({ 
+    email: Joi.string().required()
+    // Troca mensagem. Origem da linha de código: stackoverflow
+    .messages({ 'any.required': 'Some required fields are missing' }),
+     password: Joi.string().required()
+     .messages({ 'any.required': 'Some required fields are missing' }) });
+
+  const error = schema.validateAsync({ email, password });
+  console.log(error);
+ // const resonse = await models.User.create({ email, password });
+ // return resonse;
 };
 
 module.exports = {
