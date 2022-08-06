a58bc826fd4a5542ad042f8d6f56ad862fd8bfd3
Lucas <bithome1@outlook.com>
2022-08-06 17:33:05 -0300

    criado service

---
+++ src/services/putPostIdService.js
@@ -0,0 +1,24 @@
+/** source: https://sebhastian.com/sequelize-update-example/ */
+const Joi = require('joi');
+const model = require('../database/models');
+
+const putPostId = async (id, userId, { title, content }) => {
+  const schema = Joi.object({ 
+    title: Joi.string().not().empty().required()
+.messages({ 'string.empty': 'Some required fields are missing' }),
+content: Joi.string().not().empty().required()
+.messages({ 'string.empty': 'Some required fields are missing' }) });
+
+await schema.validateAsync({ title, content });
+
+  if (id !== userId) {
+    const error = new Error('Unauthorized user');
+    error.name = 'unauthorizedUserError';
+    throw error;
+  }
+  await model.BlogPost.update({ title, content }, { where: { id } });
+};
+
+module.exports = {
+  putPostId,
+};
