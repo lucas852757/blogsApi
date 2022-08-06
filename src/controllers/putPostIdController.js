266a0dd6bc56a8bdd98d13c2ae396d789c32e154
Lucas <bithome1@outlook.com>
2022-08-06 17:34:24 -0300

    controller

--- src/controllers/putPostIdController.js
+++ src/controllers/putPostIdController.js
@@ -1,4 +1,4 @@
-const service = require('../services/putPostId');
+const service = require('../services/putPostIdService');
 /**
  * 
  * @param {import('express').Request} req 
@@ -8,8 +8,9 @@ const service = require('../services/putPostId');
 module.exports = async (req, res, next) => {
   try {
     const { id } = req.params;
+    const { id: userId } = req.id;
     const { title, content } = req.body;
-    const response = service.putPostId(id, { title, content });
+    const response = service.putPostId(id, userId, { title, content });
     return res.status(200).json(response);
   } catch (error) {
     next(error);
